2cd019409716c4dcfac3b0ab6257c06aa7a946cb
DancingSnow <1121149616@qq.com>
2025-04-16 02:09:07 +0800

    实现ItemTooltip

--- src/components/CraftingPlanEntryCard.vue
+++ src/components/CraftingPlanEntryCard.vue
@@ -2,22 +2,22 @@
 import type CraftingPlanSummaryEntry from "~/core/data/ae/craft/plan/CraftingPlanSummaryEntry";
 import type jsonText from "~/core/data/minecraft/JsonText";
 
-import {computed} from "vue";
+import {computed, ref} from "vue";
 import Logger from "~/utils/Logger";
 
 const props = defineProps<{
     entry: CraftingPlanSummaryEntry
 }>()
 
-const classed = computed<string>(()=>{
-    let res:string = ''
+const classed = computed<string>(() => {
+    let res: string = ''
     if (props.entry.missingAmount) {
         res += 'card_bg_warn'
     }
     return res
 })
 
-const displayName = computed<jsonText | undefined>(()=>{
+const displayName = computed<jsonText | undefined>(() => {
     const raw = props.entry.what.displayName
     try {
         const jsonText = JSON.parse(raw)
@@ -31,17 +31,38 @@ const displayName = computed<jsonText | undefined>(()=>{
     return undefined
 })
 
+const showTooltip = ref(false)
+const mousePosition = ref({
+    x: 0,
+    y: 0
+})
+
+function onMouseMove(e: MouseEvent) {
+    mousePosition.value = {
+        x: e.x,
+        y: e.y
+    }
+}
+
+const tooltipStyle = computed<string>(() => {
+    return `position: absolute; left: ${mousePosition.value.x - 10}px; top: ${mousePosition.value.y - 10}px;`
+})
+
+const tooltips: string[] = new Array<string>()
+if (props.entry.missingAmount) {
+    tooltips.push(`缺失数量: ${props.entry.missingAmount}`)
+}
+if (props.entry.storedAmount) {
+    tooltips.push(`可用数量: ${props.entry.storedAmount}`)
+}
+if (props.entry.craftAmount) {
+    tooltips.push(`合成数量: ${props.entry.craftAmount}`)
+}
 </script>
 
 <template>
-    <el-card class="w-200px" :class="classed">
-        <div class="border_color_el mx-a mb-2 h-28px b-b b-b-solid text-end">
-            <el-text v-if="!displayName" size="small">
-                {{ props.entry.what.displayName }}
-            </el-text>
-            <JsonTextSpan v-if="displayName" :json-text="displayName" />
-        </div>
-
+    <el-card @mouseover="showTooltip = true" @mouseleave="showTooltip = false" @mousemove="onMouseMove" class="w-200px"
+             :class="classed">
         <div class="h-58px flex flex-wrap justify-end text-right">
             <div class="mx-a my-a grow">
                 <el-text
@@ -52,18 +73,18 @@ const displayName = computed<jsonText | undefined>(()=>{
                     缺失数量: {{ props.entry.missingAmount }}
                 </el-text>
                 <el-text
-                    v-if="props.entry.craftAmount"
-                    size="small"
-                    class="my-1 block"
-                >
-                    合成数量: {{ props.entry.craftAmount }}
-                </el-text>
-                <el-text
                     v-if="props.entry.storedAmount"
                     size="small"
                     class="my-1 block"
                 >
                     可用数量: {{ props.entry.storedAmount }}
+                </el-text>
+                <el-text
+                    v-if="props.entry.craftAmount"
+                    size="small"
+                    class="my-1 block"
+                >
+                    合成数量: {{ props.entry.craftAmount }}
                 </el-text>
             </div>
             <!-- height: 32px -->
@@ -72,4 +93,6 @@ const displayName = computed<jsonText | undefined>(()=>{
             </span>
         </div>
     </el-card>
+    <ItemTooltip @mousemove="onMouseMove" v-if="displayName && showTooltip" :text="displayName" :tooltips="tooltips"
+                 :style="tooltipStyle"/>
 </template>
---
+++ src/components/minecraft/ItemTooltip.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import JsonText from "~/core/data/minecraft/JsonText";
+
+const props = withDefaults(defineProps<{
+    text: JsonText
+    tooltips: string[]
+}>(), {
+    tooltips: () => new Array<string>()
+})
+</script>
+
+<template>
+    <el-card class="w-fit rounded-xl backdrop-blur-md" style="background: var(--app-tooltip-color)">
+        <JsonTextSpan :json-text="props.text" class="px-4"/>
+        <el-divider v-if="props.tooltips.length" class="m-0 my-2 b-3" style="border-color: var(--app-divider-border)"/>
+        <el-text type="info" v-for="tooltip in props.tooltips" class="block my-2">
+            {{ tooltip }}
+        </el-text>
+    </el-card>
+</template>
+
+<style scoped>
+
+</style>
