f342929da3602b16d01033eeb514db2efb0b379a
DancingSnow <1121149616@qq.com>
2025-04-16 03:21:37 +0800

    添加 formatNumber

---
+++ src/core/NumberUtil.ts
@@ -0,0 +1,17 @@
+const units: string[] = ["", "K", "M", "G", "T"]
+const pow3: number = Math.pow(10, 3)
+
+export function formatNumber(number: number): string {
+    if (number < 10000) return number.toString()
+    let unitIndex = 0
+    let temp = number
+    while (temp > pow3 && unitIndex < units.length) {
+        temp = temp / pow3
+        unitIndex++
+    }
+    if (unitIndex >= units.length) {
+        return number.toExponential(2)
+    }
+
+    return `${temp.toFixed(1)}${units[unitIndex]}`
+}
